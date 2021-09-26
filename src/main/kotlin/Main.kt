4babd328c9fe91d6e71124fe7ecd980e3bc2a8b0
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2021-09-26 12:12:00 +0200

    v0.0.3 Classes definition and added junit tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
+import java.util.logging.Logger
 
-
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+fun main()
+{
+    val logger: Logger = Logger.getLogger("Main Logger")
+    val javaVersion = System.getProperty("java.version")
+    logger.info("***********************************************")
+    logger.info("RDK - RESERVATION DEVELOPMENT KIT")
+    logger.info("Running on JVM v.$javaVersion")
+    logger.info("***********************************************")
 }
